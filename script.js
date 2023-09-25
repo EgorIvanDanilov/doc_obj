f9a05074a238742286a3e4dc4b2666af37b0ae17
Egor Danilov <mellowd94@gmail.com>
2023-09-25 19:25:39 +0200

    First hour

---
+++ script.js
@@ -0,0 +1,15 @@
+//вывод данных в консоль
+console.log("Hello,JS");
+// первый способ объявления переменной
+let x = 2;
+x = 4;
+console.log("x ==>", x);
+// второй способ объявления переменной
+// const y = 10;
+// console.log("y ==>".y);
+// DOM Document Object Model
+const btnElement = document.getElementById("btn1");
+console.log("btnElement", btnElement);
+let pElements;
+pElements = document.getElementsByTagName("p");
+console.log("pEements", pElements);
